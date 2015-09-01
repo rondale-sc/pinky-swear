1af956aad6ca72a4cc73334f63c5db648242e98d
Jonathan Jackson <dev+rondale-sc@hashrocket.com>
2015-09-01 09:25:42 -0400

    Initial then implementation

--- tests/pinky-swear-test.js
+++ tests/pinky-swear-test.js
@@ -1,8 +1,48 @@
-import adapter from '../pinky-swear/pinky-swear';
-import { expect } from 'chai';
+import { deferred } from '../pinky-swear/pinky-swear';
+import { expect, assert } from 'chai';
 
-describe('test', function(){
-  it('foo', function(){
-    // debugger
+const PENDING = 0;
+const FULFILLED = 1;
+const REJECTED = 2;
+
+describe('When fulfilled, a promise', function(){
+  it('must not transition to any other state.', function(){
+    const { promise, resolve, reject } = deferred();
+    resolve('foo');
+    resolve('bar');
+    expect(promise._result).to.equal('foo');
+    expect(promise._state).to.equal(FULFILLED);
+  });
+});
+
+describe('When fulfilled, a promise', function(){
+  it('must not transition to any other state.', function(){
+    const { promise, resolve, reject } = deferred();
+    reject('foo');
+    reject('bar');
+    expect(promise._result).to.equal('foo');
+    expect(promise._state).to.equal(REJECTED);
+  });
+});
+
+describe('When pending, a promise', function(){
+  it('may transition to FULFILLED OR REJECTED', function(){
+    const { promise, resolve, reject } = deferred();
+    expect(promise._state).to.equal(PENDING);
+    resolve('foo');
+    expect(promise._state).to.equal(FULFILLED);
+  });
+});
+
+describe('then', function(){
+  it('', function(done){
+    const { promise, resolve, reject } = deferred();
+
+    promise.then(function(foo){
+      expect(foo).to.equal('foo');
+      done();
+    }, null);
+
+    resolve('foo');
   });
 });
